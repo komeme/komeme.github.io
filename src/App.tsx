ce74a3a7e2e84e49add6877c3e065736521601d6
comeme <developer.k.mshr0316@gmail.com>
2020-03-02 00:13:51 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import {Home} from "./home";
 import {Portfolio} from "./portfolio";
 
